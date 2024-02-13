55cde247b804d17e17b02d8ea43020f03e3ffce8
hholb <hholbroo@unca.edu>
2024-02-13 11:00:33 -0500

    Convert to React

--- js/storyHTML.js
+++
@@ -1,9 +0,0 @@
-export function storyToHTML(story) {
-    return `
-        <div class="story-card">
-            <img class="profile-picture" src="${story.user.image_url}"
-             alt="profile picture">
-            <p class="username">${story.user.username}</p>
-        </div>
-    `;
-}
